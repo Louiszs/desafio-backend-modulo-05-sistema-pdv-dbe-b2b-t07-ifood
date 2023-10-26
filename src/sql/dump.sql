fd9f1044e00565d6d81c2724362089d42dca5efc
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-10-26 18:55:20 -0300

    feat(dump.sql): Adiciona tabelas iniciais do banco

---
+++ src/sql/dump.sql
@@ -0,0 +1,10 @@
+create table
+  usuarios (
+    id serial primary key,
+    nome text not null,
+    email text not null unique,
+    senha text not null
+  );
+
+create table
+  categorias (id serial primary key, descricao text);
