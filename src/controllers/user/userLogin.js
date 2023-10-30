d117201533ec3ea34a138223c649667c22c896d3
Eduardo <eduardo.htluiz@outlook.com>
2023-10-29 22:10:24 -0400

    fix(userLogin) remoção de linhas de códigos desnecessárias

--- src/controllers/user/userLogin.js
+++ src/controllers/user/userLogin.js
@@ -8,13 +8,6 @@ const userLogin = async (req, res) => {
 
     const resposta = await loginUserAndReturn(email, senha)
 
-    // if (!usuario) {
-    //   return res.status(404).json({ mensagem: 'Email ou senha inválida' });
-    // }
-    // if (!senhavalida) {
-    //   return res.status(400).json({ mensagem: 'Email ou senha inválida' });
-    // }
-
     return res.json(resposta);
   } catch (error) {
     handleError(res, error, 400);
