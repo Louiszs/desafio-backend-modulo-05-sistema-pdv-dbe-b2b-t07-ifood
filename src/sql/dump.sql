4bc97808c089bd3332fcd628b4af33908a7ebaf2
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-10 17:25:43 -0300

    update(dump.sql): Adicionando tabela de produtos e clientes

--- src/sql/dump.sql
+++ src/sql/dump.sql
@@ -19,3 +19,24 @@ insert into categorias (descricao) values
   ('Moda'),
   ('Bebê'),
   ('Games');
+
+CREATE TABLE produtos (
+    id SERIAL PRIMARY KEY,
+    descricao VARCHAR(255) NOT NULL,
+    quantidade_estoque INTEGER NOT NULL,
+    valor FLOAT NOT NULL,
+    categoria_id INTEGER REFERENCES categorias(id) NOT NULL
+);
+
+CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    cpf VARCHAR(14) UNIQUE NOT NULL,
+    cep VARCHAR(10) NOT NULL,
+    rua VARCHAR(255) NOT NULL,
+    numero VARCHAR(20) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    estado VARCHAR(100) NOT NULL
+);
